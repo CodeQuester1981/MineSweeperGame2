7c7610cb6e0f730408da0f22a4aa9af865b5d589
Clarinda <clarinda.vanwyk@gmail.com>
2018-10-25 13:45:41 +0200

    C# Minesweeper Game

---
+++ MineSweeperGame2/MinesweeperForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace MineSweeperGame2
+{
+    partial class MinesweeperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MinesweeperForm));
+            this.tileGrid = new MineSweeperGame2.MinesweeperForm.TileGrid();
+            this.gameButton = new System.Windows.Forms.PictureBox();
+            this.flagCounter = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip_Game = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip_Game_Beginner = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip_Game_Intermediate = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip_Game_Expert = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.gameButton)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tileGrid
+            // 
+            this.tileGrid.Location = new System.Drawing.Point(12, 56);
+            this.tileGrid.Name = "tileGrid";
+            this.tileGrid.Size = new System.Drawing.Size(339, 235);
+            this.tileGrid.TabIndex = 0;
+            // 
+            // gameButton
+            // 
+            this.gameButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.gameButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gameButton.BackgroundImage")));
+            this.gameButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.gameButton.Location = new System.Drawing.Point(159, 5);
+            this.gameButton.Name = "gameButton";
+            this.gameButton.Size = new System.Drawing.Size(45, 45);
+            this.gameButton.TabIndex = 1;
+            this.gameButton.TabStop = false;
+            this.gameButton.Click += new System.EventHandler(this.LoadGame);
+            // 
+            // flagCounter
+            // 
+            this.flagCounter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flagCounter.AutoSize = true;
+            this.flagCounter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flagCounter.Font = new System.Drawing.Font("Book Antiqua", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flagCounter.Location = new System.Drawing.Point(271, 13);
+            this.flagCounter.Name = "flagCounter";
+            this.flagCounter.Size = new System.Drawing.Size(80, 31);
+            this.flagCounter.TabIndex = 2;
+            this.flagCounter.Text = "label1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip_Game});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(363, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip_Game
+            // 
+            this.menuStrip_Game.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip_Game_Beginner,
+            this.menuStrip_Game_Intermediate,
+            this.menuStrip_Game_Expert});
+            this.menuStrip_Game.Name = "menuStrip_Game";
+            this.menuStrip_Game.Size = new System.Drawing.Size(50, 20);
+            this.menuStrip_Game.Text = "Game";
+            this.menuStrip_Game.Click += new System.EventHandler(this.MenuStrip_Game_Click);
+            // 
+            // menuStrip_Game_Beginner
+            // 
+            this.menuStrip_Game_Beginner.Name = "menuStrip_Game_Beginner";
+            this.menuStrip_Game_Beginner.Size = new System.Drawing.Size(141, 22);
+            this.menuStrip_Game_Beginner.Tag = "Beginner";
+            this.menuStrip_Game_Beginner.Text = "Beginner";
+            this.menuStrip_Game_Beginner.Click += new System.EventHandler(this.MenuStrip_Game_DifficultyChange);
+            // 
+            // menuStrip_Game_Intermediate
+            // 
+            this.menuStrip_Game_Intermediate.Name = "menuStrip_Game_Intermediate";
+            this.menuStrip_Game_Intermediate.Size = new System.Drawing.Size(141, 22);
+            this.menuStrip_Game_Intermediate.Tag = "Intermediate";
+            this.menuStrip_Game_Intermediate.Text = "Intermediate";
+            this.menuStrip_Game_Intermediate.Click += new System.EventHandler(this.MenuStrip_Game_DifficultyChange);
+            // 
+            // menuStrip_Game_Expert
+            // 
+            this.menuStrip_Game_Expert.Name = "menuStrip_Game_Expert";
+            this.menuStrip_Game_Expert.Size = new System.Drawing.Size(141, 22);
+            this.menuStrip_Game_Expert.Tag = "Expert";
+            this.menuStrip_Game_Expert.Text = "Expert";
+            this.menuStrip_Game_Expert.Click += new System.EventHandler(this.MenuStrip_Game_DifficultyChange);
+            // 
+            // MinesweeperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 313);
+            this.Controls.Add(this.flagCounter);
+            this.Controls.Add(this.tileGrid);
+            this.Controls.Add(this.gameButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MinesweeperForm";
+            this.Text = "Minesweeper";
+            ((System.ComponentModel.ISupportInitialize)(this.gameButton)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TileGrid tileGrid;
+        private System.Windows.Forms.PictureBox gameButton;
+        private System.Windows.Forms.Label flagCounter;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuStrip_Game;
+        private System.Windows.Forms.ToolStripMenuItem menuStrip_Game_Beginner;
+        private System.Windows.Forms.ToolStripMenuItem menuStrip_Game_Intermediate;
+        private System.Windows.Forms.ToolStripMenuItem menuStrip_Game_Expert;
+        
+    }
+}
+
